e1a88cf67f1784347bf7c0986f7e7a067a0d9306
unknown <21522425@gm.uit.edu.vn>
2024-09-22 18:18:58 +0700

    Initial Code

---
+++ carbon_credit_trading/lib/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color greenButton = Color(0xFF81C36B);
+  static const Color greyBackGround = Color(0xFFF9F9F9);
+  static const Color greenPopUpText = Color(0xFF14AE5C);
+}
---
+++ carbon_credit_trading/lib/theme/text_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle heading =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.w400, color: Colors.black);
+
+  static const TextStyle bodyText = TextStyle(
+      fontSize: 16, fontWeight: FontWeight.normal, color: Colors.black);
+  static const TextStyle buttonText =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.white);
+}
